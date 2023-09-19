3f8257ff09a482a7bb5422352aa0619b7ed73837
lin.xii <lin.xii@outlook.com>
2023-09-19 18:30:41 +0800

    feat: define typeDefs

---
+++ index.js
@@ -0,0 +1,14 @@
+import { ApolloServer, gql } from "apollo-server";
+
+// schema, 本质是一堆类型的定义
+// typeDefs, 本质是一堆类型的定义、组成的字符串。其实就是schema的字符串形式
+// 其实，算是一个东西
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
