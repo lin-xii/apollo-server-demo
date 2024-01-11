82cd860d10f42706f0551cbf0635deabba21d8ed
林十二XII <lin.xii@outlook.com>
2024-01-11 15:41:48 +0800

    feat: @cacheControl必须声明每一个字段

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -33,10 +33,45 @@ const resolvers = {
     IP: 1,
     WEB: 2,
   },
+  Library: {
+    name: (parent, args, context, { cacheControl }) => {
+      // cacheControl.setCacheHint({ maxAge: 5 });
+      // console.log(parent);
+      // console.log(info.cacheControl);
+      console.log("Library.name", cacheControl.cacheHint.maxAge);
+      return generateRandomBookTitle();
+    },
+    books: (parent, args, context, { cacheControl }) => {
+      // console.log(info.cacheControl);
+      console.log("Library.books", cacheControl.cacheHint.maxAge);
+      return [
+        {
+          title: generateRandomBookTitle(),
+          author: Math.random().toFixed(2),
+        },
+      ];
+    },
+  },
   Query: {
     books: () => {
       console.log("books");
       return books;
+    },
+    library: (_, args, context, { cacheControl }) => {
+      // console.log(info.cacheControl);
+      console.log("library", cacheControl.cacheHint.maxAge);
+      return [{}];
+      return [
+        {
+          name: generateRandomBookTitle(),
+          books: [
+            {
+              title: generateRandomBookTitle(),
+              author: Math.random().toFixed(2),
+            },
+          ],
+        },
+      ];
     },
     favoriteColor: () => "#00f",
     commonVulnType: () => 2,
@@ -47,3 +82,49 @@ const resolvers = {
 };
 
 export default resolvers;
+
+function generateRandomBookTitle() {
+  const adjectives = [
+    "Amazing",
+    "Incredible",
+    "Fantastic",
+    "Magical",
+    "Mysterious",
+    "Secret",
+    "Hidden",
+    "Lost",
+    "Unseen",
+    "Forgotten",
+  ];
+  const nouns = [
+    "Journey",
+    "Quest",
+    "Adventure",
+    "Discovery",
+    "Secret",
+    "Treasure",
+    "Realm",
+    "World",
+    "Dream",
+    "Destiny",
+  ];
+  const themes = [
+    "of Time",
+    "of Space",
+    "of Love",
+    "of Destiny",
+    "of Dreams",
+    "of Magic",
+    "of Shadows",
+    "of Light",
+    "of the Unknown",
+    "of the Ancients",
+  ];
+
+  const randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+
+  return `${randomAdjective} ${randomNoun} ${randomTheme}`;
+}
