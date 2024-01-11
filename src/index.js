2282bd9e5ae6af12105f783c611b38063192a52c
林十二XII <lin.xii@outlook.com>
2024-01-11 11:17:30 +0800

    feat: 拆分schema

--- src/index.js
+++ src/index.js
@@ -4,43 +4,7 @@ import cors from "cors";
 import http from "http";
 import express from "express";
 import { ApolloServerPluginInlineTrace } from "@apollo/server/plugin/inlineTrace";
-
-// schema, 本质是一堆类型的定义
-// typeDefs, 本质是一堆类型的定义、组成的字符串。其实就是schema的字符串形式
-// 其实，算是一个东西
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-    favoriteColor: AllowedColor
-    commonVulnType: VulnType
-    colors: ColorHex
-  }
-  enum AllowedColor {
-    RED
-    GREEN
-    BLUE
-  }
-  type EnabledColor implements ColorHex {
-    hex: String
-    enabled: Boolean
-  }
-  type DisabledColor implements ColorHex {
-    hex: String
-    disabled: Boolean
-  }
-  enum VulnType {
-    IP
-    WEB
-  }
-  union AllColor = EnabledColor | DisabledColor
-  interface ColorHex {
-    hex: String
-  }
-`;
+import typeDefs from "./schemas/index.js";
 
 const books = [
   {
@@ -52,7 +16,6 @@ const books = [
     author: "Paul Auster",
   },
 ];
-
 const resolvers = {
   AllowedColor: {
     RED: "#f00",
