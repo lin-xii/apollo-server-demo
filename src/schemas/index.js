a0db948b8716b0cfded74bf1d35a26d1f41fec0b
林十二XII <lin.xii@outlook.com>
2024-01-11 11:33:54 +0800

    feat: 将graphql拆分为单独的文件

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,38 +1,11 @@
+import { readFileSync } from "fs";
 // schema, 本质是一堆类型的定义
 // typeDefs, 本质是一堆类型的定义、组成的字符串。其实就是schema的字符串形式
 // 其实，算是一个东西
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-    favoriteColor: AllowedColor
-    commonVulnType: VulnType
-    colors: ColorHex
-  }
-  enum AllowedColor {
-    RED
-    GREEN
-    BLUE
-  }
-  type EnabledColor implements ColorHex {
-    hex: String
-    enabled: Boolean
-  }
-  type DisabledColor implements ColorHex {
-    hex: String
-    disabled: Boolean
-  }
-  enum VulnType {
-    IP
-    WEB
-  }
-  union AllColor = EnabledColor | DisabledColor
-  interface ColorHex {
-    hex: String
-  }
-`;
+
+const typeDefs = readFileSync("src/schemas/query.gql", { encoding: "utf-8" });
+// console.log(process.cwd());
+// readFileSync,是root directory下的相对路径。
+// cwd: code/apollo-server-demo
 
 export default typeDefs;
