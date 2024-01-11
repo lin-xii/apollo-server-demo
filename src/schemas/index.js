b5bdae8db1ede97cbc787c0f7bf4dbecaab07579
林十二XII <lin.xii@outlook.com>
2024-01-11 12:16:42 +0800

    feat: 递归读取当前目录的gql

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,11 +1,27 @@
-import { readFileSync } from "fs";
+import fs from "fs";
+import path from "path";
 // schema, 本质是一堆类型的定义
 // typeDefs, 本质是一堆类型的定义、组成的字符串。其实就是schema的字符串形式
 // 其实，算是一个东西
 
-const typeDefs = readFileSync("src/schemas/query.gql", { encoding: "utf-8" });
-// console.log(process.cwd());
-// readFileSync,是root directory下的相对路径。
-// cwd: code/apollo-server-demo
+const typeDefs = readCwdFiles("src/schemas");
+
+function readCwdFiles(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach((file) => {
+    const filename = path.join(dir, file);
+    const stat = fs.statSync(filename);
+    if (stat && stat.isDirectory()) {
+      /* 递归读取子目录 */
+      results = results.concat(readCwdFiles(file));
+    } else if (path.extname(file) === ".gql") {
+      /* 读取 .gql 文件内容 */
+      const content = fs.readFileSync(filename, { encoding: "utf-8" });
+      results.push(content);
+    }
+  });
+  return results;
+}
 
 export default typeDefs;
